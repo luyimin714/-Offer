c2a8305451575884cb44c21c92178bae1b9310cc
luyimin714 <luyimin714@qq.com>
2018-12-05 10:26:18 +0800

    new

--- src/Question38.java
+++ src/Question38.java
@@ -1,27 +1,56 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Question38 {
-    private int count;
-
-    public void permutation(String str) {
-        if (str == null)
-            return;
-        count = 0;
-        permutation(str.toCharArray(), 0);
-        System.out.println(count);
+    /*
+     * 字符串的排列
+     * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
+     * 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+     */
+    //leetcode 回溯法
+    public ArrayList<String> Permutation(String str) {
+        ArrayList<String> res = new ArrayList<>();
+        if (str == null || str.length() == 0) return res;
+        backtrack(res, new StringBuilder(), str.toCharArray(), new boolean[str.length()]);
+        return res;
     }
 
-    private void permutation(char[] str, int begin) {
+    private void backtrack(ArrayList<String> res, StringBuilder sb, char[] chars, boolean[] used) {
+        if (sb.length() == chars.length) {
+            res.add(sb.toString());
+        } else {
+            for (int i = 0; i < chars.length; i++) {
+                if (used[i] || (i > 0 && chars[i] == chars[i-1] && !used[i-1])) continue;
+                used[i] = true;
+                sb.append(chars[i]);
+                backtrack(res, sb, chars, used);
+                used[i] = false;
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+
+    //剑指Offer
+    public ArrayList<String> Permutation2(String str) {
+        if (str.length() == 0) return new ArrayList<>();
+        ArrayList<String> res = new ArrayList<>();
+        char[] chars = str.toCharArray();
+        permutation(res, chars, 0);
+        Collections.sort(res);
+        return res;
+    }
+
+    private void permutation(ArrayList<String> res, char[] str, int begin) {
         if (begin == str.length) {
-            System.out.println(String.valueOf(str));
-            count++;
+            String val = String.valueOf(str);
+            if (!res.contains(val))
+                res.add(val);
         } else {
             for (int i = begin; i < str.length; i++) {
                 swap(str, i, begin);
-
-                permutation(str, begin + 1);
-
+                permutation(res, str, begin + 1);
                 swap(str, i, begin);
             }
         }
@@ -33,20 +62,17 @@ public class Question38 {
         chars[j] = temp;
     }
 
+    //求所有字符的组合
     public void combination(char[] chars) {
-        if (chars == null || chars.length == 0)
-            return;
-        count = 0;
+        if (chars == null || chars.length == 0) return;
         StringBuilder str = new StringBuilder();
         for (int i = 1; i <= chars.length; i++) {
             combination(chars, 0, i, str);
         }
-        System.out.println(count);
     }
 
     private void combination(char[] chars, int begin, int m, StringBuilder str) {
         if (m == 0) {
-            count++;
             System.out.println(str);
             return;
         }
@@ -59,15 +85,5 @@ public class Question38 {
         str.deleteCharAt(str.length() - 1);
         //求n-1个字符中长度为m的组合
         combination(chars, begin + 1, m, str);
-    }
-
-    public static void main(String[] args) {
-        Question38 print = new Question38();
-        print.permutation("abc");
-
-        System.out.println();
-
-        char[] chars = {'a', 'b', 'c'};
-        print.combination(chars);
     }
 }
--- src/Question62.java
+++ src/Question62.java
@@ -6,19 +6,18 @@ public class Question62 {
      *  圆圈中最后剩下的数
      */
     public int LastRemaining_Solution(int n, int m) {
-        if (n < 1 || m < 1)
-            return -1;
+        if (n < 1 || m < 1) return -1;
 
         int last = 0;
         for (int i = 2; i <= n; i++) {
             last = (last + m) % i;
         }
+
         return last;
     }
 
     public int LastRemaining_Solution2(int n, int m) {
-        if (n < 1 || m < 1)
-            return -1;
+        if (n < 1 || m < 1) return -1;
 
         List<Integer> list = new LinkedList<>();
         for (int i = 0; i < n; i++)
@@ -29,6 +28,7 @@ public class Question62 {
             list.remove(cur);
             cur = (cur + (m - 1)) % list.size();
         }
+
         return list.get(0);
     }
 }
